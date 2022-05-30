d8a695636e865b6424bcc40c80321c698644daa0
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2022-05-30 10:20:23 +0530

    Basic Prop Types done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
+  const personName = {
+    first: "Sudeep",
+    last: "Kumar",
+  };
+
+  const nameList = [
+    {
+      first: "Sudeep",
+      last: "Kumar",
+    },
+    {
+      first: "Sandeep",
+      last: "Kumar",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet name="Sudeep" messageCount={10} isLoggedIn={false} /> */}
+      {/* <Person name={personName} /> */}
+      {/* <PersonList names={nameList} /> */}
+      {/* <Status status="loading" /> */}
+      {/* <Heading>PlaceHolder Text</Heading> */}
+      {/* <Oscar>
+        <Heading>Oscar goes to Leanardo Dicpario!</Heading>
+      </Oscar> */}
+      <Greet name="Sudeep" messageCount={10} isLoggedIn={false} />
     </div>
   );
 }
